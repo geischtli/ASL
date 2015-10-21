a20fb2e742576c9a0e682a641b53b72f4d836623
geischtli <sanhuber@student.ethz.ch>
2015-10-21 10:20:16 +0200

    fixed some aws stuff, changed from scp to rsync

--- db_starter_package/upload_init.sh
+++ db_starter_package/upload_init.sh
@@ -9,7 +9,8 @@ while getopts "a:" opt; do
   esac
 done
 
-scp -i ../../../frankfurt_key.pem setup_database.sh ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
-scp -i ../../../frankfurt_key.pem postgresql-9.4.4.tar.bz2 ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
-scp -i ../../../frankfurt_key.pem ../initDatabase.sql ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
-scp -i ../../../frankfurt_key.pem setup_mydb.sh ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+rsync -avze 'ssh -i ../../../frankfurt_key.pem' setup_database.sh ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+rsync -avze 'ssh -i ../../../frankfurt_key.pem' postgresql-9.4.4.tar.bz2 ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+rsync -avze 'ssh -i ../../../frankfurt_key.pem' ../initDatabase.sql ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+rsync -avze 'ssh -i ../../../frankfurt_key.pem' setup_mydb.sh ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+
--- upload_project.sh
+++ upload_project.sh
@@ -10,4 +10,5 @@ while getopts "a:" opt; do
 done
 
 #scp pg_hba.conf sanhuber@dryad12.ethz.ch:/mnt/local/sanhuber/postgres/db
-scp -r -i ../../frankfurt_key.pem . ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+#scp -r -i ../../frankfurt_key.pem . ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+rsync -avze 'ssh -i ../../frankfurt_key.pem' --exclude=.git --exclude=db_starter_package --exclude=logs . ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/
