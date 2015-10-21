02c24aaaaa8492b840551593b164c3fc7352955a
geischtli <sanhuber@student.ethz.ch>
2015-10-21 12:37:41 +0200

    /home/ missed

--- db_starter_package/upload_finish.sh
+++ db_starter_package/upload_finish.sh
@@ -15,4 +15,4 @@ mv pg_hba.conf /home/ec2-user/postgres/db
 
 #scp postgresql.conf sanhuber@dryad12.ethz.ch:/mnt/local/sanhuber/postgres/db
 #rsync -avze 'ssh -i ../../../frankfurt_key.pem' postgresql.conf ec2-user@ec2-$IP.eu-central-1.compute.amazonaws.com:/home/ec2-user/postgres/db
-mv postgresql.conf ec2-user/postgres/db
+mv postgresql.conf /home/ec2-user/postgres/db
