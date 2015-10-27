51fbb0ef61e6f8eac07bb1283e920a4e6f0510d6
geischtli <sanhuber@student.ethz.ch>
2015-10-27 16:48:42 +0100

    updated bench level 3

--- db_baseline/benchScripts/benchLevel3.sql
+++ db_baseline/benchScripts/benchLevel3.sql
@@ -1,10 +1,14 @@
 -- this bench level is used to measure the throughtput
--- and latency behaviour when repeatidly inserting
--- new messages into the system. The main interest
--- is about where the database begins to slow down
--- expects 100 clients and queues in the database registered
+-- and latency behaviour when performing the most
+-- consting operation, i.e. the top remove when the
+-- database is consistently growing by 1 message
+-- per this file transaction.
 BEGIN
-	\setrandom myClientId 1 100
-	\setrandom queue 1 100
-	SELECT * FROM send_message(:myClientId, :myClientId, :queue, 'abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0');
+	\setrandom myClientIdA 1 100
+	\setrandom myClientIdB 1 100
+	\setrandom queueA 1 100
+	\setrandom queueB 1 100
+	SELECT * FROM send_message(:myClientIdA, :myClientIdA, :queueA, 'abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0');
+	SELECT * FROM send_message(:myClientIdB, :myClientIdB, :queueB, 'abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0abcdhjels0');
+	SELECT * FROM remove_top_message_from_queue(:myClientIdA, :queueA);
 END;
