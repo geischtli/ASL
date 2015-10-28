55468326281b2a06832c7a8df22ccaa89e02f866
geischtli <sanhuber@student.ethz.ch>
2015-10-28 13:02:26 +0100

    new mw ip

---
+++ plotting tools/readTpAndRtt.m
@@ -0,0 +1,3 @@
+clear variables
+%ctp = dlmread('C:\Users\Sandro\Documents\ASL_LOGS\mw_baseline_30C_30DBC\throughput.log');
+rtt = dlmread('C:\Users\Sandro\Documents\ASL_LOGS\mw_baseline_30C_30DBC\rtt.log');
