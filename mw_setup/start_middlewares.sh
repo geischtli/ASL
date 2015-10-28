49a90a350b5701127a803b35b12f30e011fd0710
geischtli <sanhuber@student.ethz.ch>
2015-10-28 10:22:49 +0100

    call mw not async anymore (no &)

--- mw_setup/start_middlewares.sh
+++ mw_setup/start_middlewares.sh
@@ -18,8 +18,13 @@ printf "\n"
 echo Number of Middlewares: $NUMMIDDLEWARES
 printf "\n"
 
-COUNTER=0
-while [  $COUNTER -lt $NUMMIDDLEWARES ]; do
-	ant -f antBuildMiddleware.xml &
-	COUNTER=`expr $COUNTER + 1`
-done
+# we want to run only 1 middleware per machine anyway
+# so we can run it synchronously without any problems.
+ant -f antBuildMiddleware.xml
+
+# old impl...
+#COUNTER=0
+#while [  $COUNTER -lt $NUMMIDDLEWARES ]; do
+#	ant -f antBuildMiddleware.xml &
+#	COUNTER=`expr $COUNTER + 1`
+#done
