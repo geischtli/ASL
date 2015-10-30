1b8169a9bfe0dd58355c18000e90db6ac1586aea
geischtli <sanhuber@student.ethz.ch>
2015-10-30 17:11:48 +0100

    finished client baseline plottings

--- plotting tools/client_baseline.m
+++ plotting tools/client_baseline.m
@@ -20,15 +20,15 @@ for k = 1:2
         numClients = 2^i;
         currDir = strcat(basedir, strcat(num2str(numClients), 'C\'));
         currFile = strcat(currDir, 'clientTimes.log');
-        clientTimes = dlmread(currFile);
+        mwTimes = dlmread(currFile);
         % delete all rows with 0's (mostly first few measurements)
-        clientTimes(~all(clientTimes, 2), :) = [];
-        numRows = size(clientTimes, 1);
+        mwTimes(~all(mwTimes, 2), :) = [];
+        numRows = size(mwTimes, 1);
         % go through all lines and sum up each column for every second
         secondsIndices = ones(numClients, 1);
         secondsData = zeros(ceil(numRows/numClients), 3);
         for j = 1:numRows
-            currRow = clientTimes(j, :);
+            currRow = mwTimes(j, :);
             currClient = currRow(1);
             % don't count warm-up and cool-down phase
             if (secondsIndices(currClient) >= minSecondsIdx && ...
@@ -60,6 +60,53 @@ for k = 1:2
         reqPerSec2000 = reqPerSec;
         writePerSec2000 = writePerSec;
         readPerSec2000 = readPerSec;
+    end
+    basedir = 'C:\Users\Sandro\Documents\ASL_LOGS\client_baseline_';
+    msgLen = '2000\';
+end
+
+msgLen = '200\';
+for k = 1:2
+    basedir = strcat(basedir, msgLen);
+
+    indices = [];
+    reqPerSec = [];
+    writePerSec = [];
+    readPerSec = [];
+    minSecondsIdx = 10;
+    maxSecondsIdx = 26;
+    if k == 1
+        maxFolderIdx = 9;
+    else
+        maxFolderIdx = 8;
+    end
+    for i = 0:maxFolderIdx
+        numClients = 2^i;
+        currDir = strcat(basedir, strcat(num2str(numClients), 'C\'));
+        currFile = strcat(currDir, 'middlewareTimes.log');
+        mwTimes = dlmread(currFile);
+        % delete all rows with 0's (mostly first few measurements)
+        mwTimes(~all(mwTimes, 2), :) = [];
+        numRows = size(mwTimes, 1);
+        secondsData = mwTimes(minSecondsIdx:(numRows-minSecondsIdx), :);
+        numEffectiveRows = size(secondsData, 1);
+        % this data all belongs to the 2^i testset
+        indices(end+1:(end+numEffectiveRows), :) = numClients;
+        reqPerSec(end+1:(end+numEffectiveRows), :) = secondsData(:, 1);
+        writePerSec(end+1:(end+numEffectiveRows), :) = secondsData(:, 2);
+        readPerSec(end+1:(end+numEffectiveRows), :) = secondsData(:, 3);
+    end
+
+    if strcmp(msgLen, '200\')
+        mwindices200 = indices;
+        mwreqPerSec200 = reqPerSec;
+        mwwritePerSec200 = writePerSec;
+        mwreadPerSec200 = readPerSec;
+    else
+        mwindices2000 = indices;
+        mwreqPerSec2000 = reqPerSec;
+        mwwritePerSec2000 = writePerSec;
+        mwreadPerSec2000 = readPerSec;
     end
     basedir = 'C:\Users\Sandro\Documents\ASL_LOGS\client_baseline_';
     msgLen = '2000\';
--- plotting tools/client_baseline_make_plot.m
+++ plotting tools/client_baseline_make_plot.m
@@ -65,7 +65,7 @@ title('Client Write Time')
 figure();
 hold on
 colors = ['b', 'r'];
-data = {writePerSec200, writePerSec2000};
+data = {readPerSec200, readPerSec2000};
 indices = {indices200, indices2000};
 oldnumMedians = 1;
 oldval = 0;
@@ -91,3 +91,96 @@ legend('Message size 200', 'Message size 2000', 'Location', 'northwest')
 xlabel('Number of concurrent clients')
 ylabel({'Time taken to read from the channel (ms)'; 'per second averaged over all concurrent clients'})
 title('Client Read Time')
+
+%% make the plot for middleware readPerSec
+figure();
+hold on
+colors = ['b', 'r'];
+data = {mwreqPerSec200, mwreqPerSec2000};
+indices = {mwindices200, mwindices2000};
+oldnumMedians = 1;
+oldval = 0;
+for k = 1:2
+    boxplot(cell2mat(data(k)), cell2mat(indices(k)), 'symbol', strcat(colors(k), '+'));
+    medians = findobj(gca,'tag','Median');
+    numMedians = length(medians);
+    xs = zeros(numMedians, 1);
+    ys = zeros(numMedians, 1);
+    oldnumMedians = numMedians - oldval;
+    for i = 1:oldnumMedians
+        currMedian = medians(i);
+        xt = currMedian.XData;
+        xs(i) = mean(xt);
+        yt = currMedian.YData;
+        ys(i) = mean(yt);
+    end
+    oldval = oldnumMedians;
+    plot(xs(1:oldval), ys(1:oldval), 'color', colors(k), 'linewidth', 2)
+    oldnumMedians = numMedians;
+end
+legend('Message size 200', 'Message size 2000', 'Location', 'northwest')
+xlabel('Number of concurrent clients')
+ylabel({'Number of requests completed'; 'per second averaged over all concurrent clients'})
+title('Middleware Throughput')
+
+%% make the plot for writePerSec
+figure();
+hold on
+colors = ['b', 'r'];
+data = {mwwritePerSec200, mwwritePerSec2000};
+indices = {mwindices200, mwindices2000};
+oldnumMedians = 1;
+oldval = 0;
+for k = 1:2
+    boxplot(cell2mat(data(k)), cell2mat(indices(k)), 'symbol', strcat(colors(k), '+'));
+    medians = findobj(gca,'tag','Median');
+    numMedians = length(medians);
+    xs = zeros(numMedians, 1);
+    ys = zeros(numMedians, 1);
+    oldnumMedians = numMedians - oldval;
+    for i = 1:oldnumMedians
+        currMedian = medians(i);
+        xt = currMedian.XData;
+        xs(i) = mean(xt);
+        yt = currMedian.YData;
+        ys(i) = mean(yt);
+    end
+    oldval = oldnumMedians;
+    plot(xs(1:oldval), ys(1:oldval), 'color', colors(k), 'linewidth', 2)
+    oldnumMedians = numMedians;
+end
+legend('Message size 200', 'Message size 2000', 'Location', 'northwest')
+xlabel('Number of concurrent clients')
+ylabel({'Time taken for writing to the channel (ms)'; 'per second averaged over all concurrent clients'})
+title('Middleware Write Time')
+
+%% make the plot for readPerSec
+figure();
+hold on
+colors = ['b', 'r'];
+data = {mwreadPerSec200, mwreadPerSec2000};
+indices = {mwindices200, mwindices2000};
+oldnumMedians = 1;
+oldval = 0;
+for k = 1:2
+    boxplot(cell2mat(data(k)), cell2mat(indices(k)), 'symbol', strcat(colors(k), '+'));
+    medians = findobj(gca,'tag','Median');
+    numMedians = length(medians);
+    xs = zeros(numMedians, 1);
+    ys = zeros(numMedians, 1);
+    oldnumMedians = numMedians - oldval;
+    for i = 1:oldnumMedians
+        currMedian = medians(i);
+        xt = currMedian.XData;
+        xs(i) = mean(xt);
+        yt = currMedian.YData;
+        ys(i) = mean(yt);
+    end
+    oldval = oldnumMedians;
+    plot(xs(1:oldval), ys(1:oldval), 'color', colors(k), 'linewidth', 2)
+    oldnumMedians = numMedians;
+end
+legend('Message size 200', 'Message size 2000', 'Location', 'northwest')
+xlabel('Number of concurrent clients')
+ylabel({'Time taken to read from the channel (ms)'; 'per second averaged over all concurrent clients'})
+title('Middleware Read Time')
