8d1340169c595fb45df93d2648fa6246583e7e90
geischtli <sanhuber@student.ethz.ch>
2015-10-31 14:57:48 +0100

    even less

--- db_baseline/benchScripts/benchLevel3_delete.sql
+++ db_baseline/benchScripts/benchLevel3_delete.sql
@@ -2,13 +2,4 @@
 -- remove top message a 100 times, to get average of this operation
 BEGIN
 	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
-	SELECT * FROM remove_top_message_from_queue(1, 1);
 END;
