02982ecc354d45aca94c38c0fd023c8ab63c192d
geischtli <sanhuber@student.ethz.ch>
2015-11-01 12:35:08 +0100

    finish db_data_baseline plot

--- plotting tools/db_data_baseline.m
+++ plotting tools/db_data_baseline.m
@@ -11,7 +11,7 @@ for file = 6:numFiles-1
     currFile = files(file).name;
     currData = dlmread(strcat(basedir, currFile));
     [m, ~] = size(currData);
-    mergedData = zeros(m, 5);
+    mergedData = zeros(m, 6);
     mergedData(:, 1:4) = currData(:, 1:4);
     mergedData(:, 5) = currData(:, 5).*10^6;
     mergedData(:, 5) = mergedData(:, 5) + currData(:, 6);
@@ -22,12 +22,75 @@ end
 % now sort by the concatenated time column
 data = sortrows(data, 5);
 
+% 6th column is used to keep track of database size
+% file index = 0 -> 10 inserts, = 1 -> 1 delete
+numRows = size(data, 1);
+for i = 2:numRows
+    if data(i, 4) == 1
+        data(i, 6) = data(i-1, 6) + 10;
+    else
+        if data(i-1, 6) > 0
+            data(i, 6) = data(i-1, 6) -1;
+        end
+    end
+end
+
+% only look at 10-second intervals (else plot is too messy)
+data(:, 5) = round(data(:, 5)./10^7);
+% start at second 0
+data(:, 5) = data(:, 5) - min(data(:, 5));
+% work with milliseconds
+data(:, 3) = round(data(:, 3)/10^3);
 % get all inserts
 inserts = data(data(:, 4) == 0, :);
 % get all deletes
 deletes = data(data(:, 4) == 1, :);
 
-% plot them
+% now plot the data
 hold on
-plot(inserts(:, 3), 'b')
-plot(deletes(:, 3), 'r')
+
+% scale the table size data such that the plot looks appealing
+% intentionally wrong output, but use text to set marks
+data(:, 6) = data(:, 6)./983.085454545455;
+boxplot(data(:, 6), data(:, 5), 'symbol', 'g+')
+
+boxplot(inserts(:, 3), inserts(:, 5), 'symbol', 'b+')
+boxplot(deletes(:, 3), deletes(:, 5), 'symbol', 'r+')
+
+% plot additionally the 50% quantile (media) for readability and
+% a nice legend reference
+medians = findobj(gca,'tag','Median');
+numMedians = length(medians)/3;
+colors = ['r', 'b', 'g'];
+for i = 1:3
+    currMedians = medians(((i-1)*numMedians + 1):(i*numMedians));
+    xs = zeros(numMedians, 1);
+    ys = zeros(numMedians, 1);
+    for j = 1:numMedians
+        currMedian = currMedians(j);
+        xt = currMedian.XData;
+        xs(j) = mean(xt);
+        yt = currMedian.YData;
+        ys(j) = mean(yt);
+    end
+    plot(xs, ys, 'color', colors(i), 'linewidth', 2)
+end
+
+% create legend
+legend('remove top message 50% quantile', ...
+    'insert 10 new messages 50% quantile', ...
+    'size of message table 50% quantile', 'Location', 'northwest')
+xlabel('10^-1 seconds (10-second intervals)')
+ylabel('Time needed to complete action (ms)')
+title('Database behaviour with data')
+
+set(gca,'Box', 'off');
+axesPosition = get(gca, 'Position');
+hNewAxes = axes('Position', axesPosition,...
+                'Color', 'none',...
+                'YLim', [0 7.4],...
+                'YAxisLocation', 'right',...
+                'XTick', [],...
+                'YTick', [1 2 3 4 5 6 7], ...
+                'Box', 'off');
+ylabel(hNewAxes,'Message Table size * 10^5(# of entries)');
