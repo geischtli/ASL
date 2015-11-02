cd06a8088536264d0d0fb4d5f98c6d1614abb51c
geischtli <sanhuber@student.ethz.ch>
2015-11-02 10:51:47 +0100

    finished dt_tp baseline plot

--- plotting tools/db_tp_lat_baseline.m
+++ plotting tools/db_tp_lat_baseline.m
@@ -10,6 +10,8 @@ numFactors = length(factors);
 
 hold on
 
+%% Throughput plot
+
 % remember how many medians were there before
 oldNumMedians = 0;
 
@@ -17,26 +19,100 @@ for level = numLevels:-1:1
     currLevel = levels(level, :);
     % read the files of the current level
     tp = dlmread(strcat(strcat(basedir, currLevel), 'tp_summary.log'));
-    lat = dlmread(strcat(strcat(basedir, currLevel), 'lat_summary.log'));
     idx = dlmread(strcat(strcat(basedir, currLevel), 'idx_summary.log'));
-    % apply factor to throughput and latency
+    % apply factor to throughput
     tp = tp * factors(level);
-    lat = lat * factors(level);
     % plot the data
     boxplot(tp, idx)
     % now add the 50% quantile line
     medians = findobj(gca,'tag','Median');
     numMedians = length(medians);
     myNumMedians = numMedians - oldNumMedians;
-    xs = zeros(myNumMedians, 1);
-    ys = zeros(myNumMedians, 1);
+    xs = [];
+    ys = [];
+    medianCounter = 1;
     for j = 1:myNumMedians
         currMedian = medians(j);
-        xt = currMedian.XData;
-        xs(j) = mean(xt);
         yt = currMedian.YData;
-        ys(j) = mean(yt);
+        yyt = mean(yt);
+        if yyt ~= 0
+            ys(medianCounter) = yyt;
+            xt = currMedian.XData;
+            xs(medianCounter) = mean(xt);
+            medianCounter = medianCounter + 1;
+        end
     end
     plot(xs, ys, 'color', colors(level, :), 'linewidth', 2)
     oldNumMedians = oldNumMedians + myNumMedians;
 end
+
+% add legend and plot description
+title('Database Throughput under different configurations and query sets')
+xlabel('Number of concurrent clients')
+ylabel('Number of SQL-statements per second')
+legend('Level 2 with index', 'Level 2 no index', 'Level 1', 'Level 0', ...
+    'Location', 'northwest')
+%modify xticks because its too messy with standard distribution
+labels = char('1', '', '', '', '5', '', '', '', '', ...
+    '10', '', '', '', '', '15', '', '', '', '',...
+    '20', '', '', '', '', '25', '', '', '', '', ...
+    '30', '', '', '', '', '35', '', '', '', '', ...
+    '40', '', '', '', '', '45', '', '', '', '', ...
+    '50', '', '', '', '', '55', '', '', '', '', ...
+    '60');
+set(gca, 'XTickLabel', labels)
+
+%% Latency plot
+hold off
+% create new window
+figure()
+hold on
+% remember how many medians were there before
+oldNumMedians = 0;
+
+for level = numLevels:-1:1
+    currLevel = levels(level, :);
+    % read the files of the current level
+    lat = dlmread(strcat(strcat(basedir, currLevel), 'lat_summary.log'));
+    idx = dlmread(strcat(strcat(basedir, currLevel), 'idx_summary.log'));
+    % apply factor to latency
+    lat = lat / factors(level);
+    % plot the data
+    boxplot(lat, idx)
+    % now add the 50% quantile line
+    medians = findobj(gca,'tag','Median');
+    numMedians = length(medians);
+    myNumMedians = numMedians - oldNumMedians;
+    xs = [];
+    ys = [];
+    medianCounter = 1;
+    for j = 1:myNumMedians
+        currMedian = medians(j);
+        yt = currMedian.YData;
+        yyt = mean(yt);
+        if yyt ~= 0
+            ys(medianCounter) = yyt;
+            xt = currMedian.XData;
+            xs(medianCounter) = mean(xt);
+            medianCounter = medianCounter + 1;
+        end
+    end
+    plot(xs, ys, 'color', colors(level, :), 'linewidth', 2)
+    oldNumMedians = oldNumMedians + myNumMedians;
+end
+
+% add legend and plot description
+title('Database Latency under different configurations and query sets')
+xlabel('Number of concurrent clients')
+ylabel('Number of SQL-statements per second')
+legend('Level 2 with index', 'Level 2 no index', 'Level 1', 'Level 0', ...
+    'Location', 'northwest')
+%modify xticks because its too messy with standard distribution
+labels = char('1', '', '', '', '5', '', '', '', '', ...
+    '10', '', '', '', '', '15', '', '', '', '',...
+    '20', '', '', '', '', '25', '', '', '', '', ...
+    '30', '', '', '', '', '35', '', '', '', '', ...
+    '40', '', '', '', '', '45', '', '', '', '', ...
+    '50', '', '', '', '', '55', '', '', '', '', ...
+    '60');
+set(gca, 'XTickLabel', labels)
