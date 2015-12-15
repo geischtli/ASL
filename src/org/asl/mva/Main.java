d7c55cb0714003d538183dc925cf0d9318d6fdd8
geischtli <sanhuber@student.ethz.ch>
2015-12-15 18:15:16 +0100

    fdsafd

--- src/org/asl/mva/Main.java
+++ src/org/asl/mva/Main.java
@@ -20,11 +20,11 @@ public class Main {
 		//DelayCenterDevice client = new DelayCenterDevice(1/47.5059, 1, N);
 		//LoadDependentDevice tp = new LoadDependentDevice(1, N, new double[]{206.6073, 206.6073, 206.6073, 206.6073, 206.6073});
 		//LoadDependentDevice db = new LoadDependentDevice(1, N, new double[]{460.8295, 460.8295, 460.8295, 460.8295, 460.8295});
-		LoadDependentDevice db = new LoadDependentDevice(1, N, new double[]{N*417.232022165388, N*414.97036957581, N*412.179989871481, N*408.955559006631,
-				N*405.389909679417, N*401.572453203383, N*397.588040395285, N*393.516232876117, N*389.430940509012, N*385.400372764355});
-		LoadDependentDevice mw = new LoadDependentDevice(1, N, new double[]{N*8409.87830457775, N*8225.07137306733, N*8062.26893987844, N*7918.38966825234,
-				N*7790.83027659137, N*7677.36448083654, N*7576.06552702905, N*7485.2461918263, N*7403.41191580775, N*7329.22396112658});
-		DelayCenterDevice client = new DelayCenterDevice(0.0035, 1, N);
+		LoadDependentDevice db = new LoadDependentDevice(1, N, new double[]{1*417.232022165388, 2*414.97036957581, 3*412.179989871481, 4*408.955559006631,
+				5*405.389909679417, 6*401.572453203383, 7*397.588040395285, 8*393.516232876117, 9*389.430940509012, N*385.400372764355});
+		LoadDependentDevice mw = new LoadDependentDevice(1, N, new double[]{1*8409.87830457775, 2*8225.07137306733, 3*8062.26893987844, 4*7918.38966825234,
+				5*7790.83027659137, 6*7677.36448083654, 7*7576.06552702905, 8*7485.2461918263, 9*7403.41191580775, N*7329.22396112658});
+		DelayCenterDevice client = new DelayCenterDevice(0.0014, 1, N);
 		// and store them in list
 		ArrayList<Device> devices = new ArrayList<Device>();
 		devices.add(mw);
